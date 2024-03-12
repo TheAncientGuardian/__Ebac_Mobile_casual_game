43fc7dcfb93f905eefbada799e42087a9a1dc160
Helio Junior <theancientguardian@protonmail.com>
2024-03-12 11:28:55 -0300

    alot of things

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //publics
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
+    public string tagToCheckEnemy = "Enemy";
+    //privates
+    private  bool _canRun;
+    private Vector3 _pos;
+    
+
+    private void Start()
     {
-        
+        _canRun =true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(!_canRun)return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            _canRun = false;
+        }
     }
 }
